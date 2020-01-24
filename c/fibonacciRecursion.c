7cc0faf1187108925df7f1e8cca057d41dbe7378
Enjoy <kanneg881@gmail.com>
2020-01-24 09:47:17 +0800

    add: c fibonacci and recursive fibonacci

---
+++ c/fibonacciRecursion.c
@@ -0,0 +1,21 @@
+/*
+ * 費伯納西的遞迴程式
+ */
+
+#include <stdio.h>
+
+int Fbi(int i) {
+  if (i < 2)
+    return i == 0 ? 0 : 1;
+
+  return Fbi(i - 1) + Fbi(i - 2);
+}
+
+int main() {
+  int i;
+
+  for (i = 0; i < 40; i++)
+    printf("%d ", Fbi(i));
+
+  return 0;
+}
