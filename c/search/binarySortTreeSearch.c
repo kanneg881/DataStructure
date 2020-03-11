effb388888f7622a441accc4aa92957fba0718c6
Enjoy <kanneg881@gmail.com>
2020-03-11 16:51:49 +0800

    add: delete function

--- c/search/binarySortTreeSearch.c
+++ c/search/binarySortTreeSearch.c
@@ -16,6 +16,65 @@ typedef struct BiTNode {
   struct BiTNode *lchild, *rchild;
 } BiTNode, *BiTree;
 
+// 從二元搜尋樹中刪除節點 p，並重接它的左或右子樹
+Status Delete(BiTree *p) {
+  BiTree q, s;
+
+  // 右子樹空則只需重接它的左子樹
+  if ((*p)->rchild == NULL) {
+    q = *p;
+    *p = (*p)->lchild;
+    free(q);
+  }
+  // 只需重接它的右子樹
+  else if ((*p)->lchild == NULL) {
+    q = *p;
+    *p = (*p)->rchild;
+    free(q);
+  }
+  // 左右子樹均不空
+  else {
+    q = *p;
+    s = (*p)->lchild;
+
+    // 轉左側，然後向右側直到盡頭（找待刪節點的前驅）
+    while (s->rchild) {
+      q = s;
+      s = s->rchild;
+    }
+    // s 指向被刪節點的直接前驅
+    (*p)->data = s->data;
+
+    if (q != *p)
+      // 重接 q 的右子樹
+      q->rchild = s->lchild;
+    else
+      // 重接 q 的左子樹
+      q->lchild = s->lchild;
+    free(s);
+  }
+  return TRUE;
+}
+
+/*
+ * 若二元搜尋樹 T 中存在鍵值等於 key 的資料元素時，則刪除
+ * 該資料元素節點，並回傳 TRUE，否則回傳 FALSE。
+ */
+Status DeleteBST(BiTree *T, int key) {
+  // 不存在鍵值等於 key 的資料元素
+  if (!*T)
+    return FALSE;
+  else {
+    // 找到鍵值等於 key 的資料元素
+    if (key == (*T)->data)
+      return Delete(T);
+    else if (key < (*T)->data)
+      return DeleteBST(&(*T)->lchild, key);
+    else
+      return DeleteBST(&(*T)->rchild, key);
+  }
+}
+
 /*
  * 遞迴搜尋二元搜尋樹 T 中是否存在 key
  * 指標 f 指向 T 的雙親，其初始調用值為 NULL
